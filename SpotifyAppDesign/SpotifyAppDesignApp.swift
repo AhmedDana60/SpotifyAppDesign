a940799a231a557a71f58597d79d44a54053c4c1
AhmedDana60 <ahmeddanamo@gmail.com>
2024-04-20 13:32:56 +0300

    Initial Commit

---
+++ SpotifyAppDesign/SpotifyAppDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAppDesignApp.swift
+//  SpotifyAppDesign
+//
+//  Created by Ahmed Dana Mohammed on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAppDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
